d6030501d4e3b2ebda811b5119b3df70b13a8896
Jonatas-Lap <jonatassilvaemanuel@gmail.com>
2024-10-18 01:16:41 -0300

    docker script

--- script.sh
+++ script.sh
@@ -1,21 +1,51 @@
-#!/usr/bin bash
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+stop_containers () {
+	echo "Stopping other docker container"
+	if [[ $(docker ps -q) ]]; then 
+		echo "Found and stopped containers" 
+		docker stop $(docker ps -q) 
+	else 
+		echo "No containers running..."
+	fi
+}
 
 # containers sub-network
-docker network create subnet=172.18.0.0/24 db-conn
+create_net () {
+	docker network create subnet=172.18.0.0/24 db-conn
+}
 
 # wordpress php server
-docker build -t  php-apache ./server
-docker run -d -p 8080:80 \
-	--network db-conn --ip 172.18.0.2 \
-	--name wordpress-apache-container php-apache
+create_images () {
+	docker build -t  php-apache ./server
+	docker build -t  php-db ./server/db
+}
 
-# mysql server
-docker build -t  php-db ./server/db
-docker run --name php-db-container -d -p 3306:3306 \
-		--network db-conn --ip 172.18.0.3 \
-		-e MYSQL_ROOT_PASSWORD=root123 --user 1000:50 php-db
+create_containers () {
+	docker run -d -p 8080:80 \
+		--network db-conn --ip 172.18.0.2 \
+		--name wordpress-apache-container php-apache
 
-# to use mysql in cli
+	docker run --name php-db-container -d -p 3306:3306 \
+			--network db-conn --ip 172.18.0.3 \
+			-e MYSQL_ROOT_PASSWORD=root123 --user 1000:50 php-db
+}
+
+start_containers () {
+	docker start wordpress-apache-container
+	docker start php-db-ip
+}
+
+case $1 in
+	"stop_containers") stop_containers ;;
+	"create_net") create_net ;;
+	"create_images") create_images ;;
+	"create_containers") create_containers ;;
+	"start_containers") start_containers ;;
+	*) echo "options[ stop_containers | create_net | create_images | create_containers | start_containers]"
+esac
+
 # docker exec -it <db-container> mysql -u root -p
-
 # docker logs <db-container>
